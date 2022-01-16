9eaf56d6f4337d581ac5a32f6fc6f1dc21db8c78
CynthiaOuma12673 <cynthia.ouma@student.moringaschool.com>
2022-01-16 21:32:49 +0300

    it will declare the variables in js

---
+++ js/index.js
@@ -0,0 +1,12 @@
+let cost,costofcrust,costoftopping;
+let total=0;
+
+function Orderpizza(flavor,size,crust,topping,total){
+    this.flavor=flavor;
+    this.size=size;
+    this.crust=crust;
+    this.topping=topping;
+    this.tital=total;
+}
+
+// validation of the next button
